aa9f0d74f677290d8fbfcf370e6cf415f9df02f1
Himanshu Gupta <himanshg@yahoo-inc.com>
2014-08-22 09:08:01 -0500

    printing the yjava_daemon start command on startup for druid packages

--- digits-package/src/main/scripts/start_coordinator.sh
+++ digits-package/src/main/scripts/start_coordinator.sh
@@ -9,6 +9,6 @@ CLASSPATH=$YINST_ROOT/conf/${YINST_VAR_basePkg}/coordinator
 
 JVM_ARGS="-Xmx${greyhawk_edigits_druid__coordinatorMaxMem} ${greyhawk_edigits_druid__coordinatorJavaOpts} -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Djava.io.tmpdir=$YINST_ROOT/tmp/$YINST_VAR_basePkg ${JVM_ARGS}"
 
-. yjava_daemon_start.sh
+. ${YINST_ROOT}/share/${YINST_VAR_basePkg}/scripts/yjava_daemon_start.sh
 
 exit $?
