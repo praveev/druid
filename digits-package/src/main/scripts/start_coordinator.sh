a63931fcbd3b2b8912d99d035d8a7f4a9f503c95
Himanshu Gupta <himanshg@yahoo-inc.com>
2014-09-11 11:06:53 -0500

    exporting kinit credentials in env variables(recommended way to do kinit by hadoop team) instead of scheduling yinst cron job to call kinit command which breaks if yinst cron start is not called appropriately and sometimes for unknown reasons

--- digits-package/src/main/scripts/start_coordinator.sh
+++ digits-package/src/main/scripts/start_coordinator.sh
@@ -3,6 +3,7 @@
 COMPONENT_NAME=druid_coordinator
 APPLICATION_ARGS="server coordinator"
 USER=${greyhawk_edigits_druid__headlessUser}
+KEYDB_FILE=${greyhawk_edigits_druid__keydbFile}
 
 # put broker config in class path
 CLASSPATH=$YINST_ROOT/conf/${YINST_VAR_basePkg}/coordinator
--- digits-package/src/main/scripts/start_historical.sh
+++ digits-package/src/main/scripts/start_historical.sh
@@ -3,6 +3,7 @@
 COMPONENT_NAME=druid_historical
 APPLICATION_ARGS="server historical"
 USER=${greyhawk_edigits_druid__headlessUser}
+KEYDB_FILE=${greyhawk_edigits_druid__keydbFile}
 
 # put broker config in class path
 CLASSPATH=$YINST_ROOT/conf/${YINST_VAR_basePkg}/historical
