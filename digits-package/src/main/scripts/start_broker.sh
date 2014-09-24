a63be6e3ba1833bcc541de5dee2e0ecdf45977ba
mclawhor <mclawhor@yahoo-inc.com>
2014-09-24 14:28:55 -0500

    Replaced historicalDirectMem property with brokerDirectMem property and made optional in start script.

--- digits-package/src/main/scripts/start_broker.sh
+++ digits-package/src/main/scripts/start_broker.sh
@@ -8,7 +8,11 @@ KEYDB_FILE=${greyhawk_edigits_druid__keydbFile}
 # put broker config in class path
 CLASSPATH=$YINST_ROOT/conf/${YINST_VAR_basePkg}/broker
 
-JVM_ARGS="-XX:MaxDirectMemorySize=${greyhawk_edigits_druid__historicalDirectMem} -Xmx${greyhawk_edigits_druid__brokerMaxMem} ${greyhawk_edigits_druid__brokerJavaOpts} -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Djava.io.tmpdir=$YINST_ROOT/tmp/$YINST_VAR_basePkg ${JVM_ARGS}"
+JVM_ARGS=" -Xmx${greyhawk_edigits_druid__brokerMaxMem} ${greyhawk_edigits_druid__brokerJavaOpts} -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Djava.io.tmpdir=$YINST_ROOT/tmp/$YINST_VAR_basePkg ${JVM_ARGS}"
+
+if [ -n "$greyhawk_edigits_druid__brokerDirectMem" ]; then
+    JVM_ARGS = "-XX:MaxDirectMemorySize=${greyhawk_edigits_druid__brokerDirectMem} $JVM_ARGS"
+fi
 
 . ${YINST_ROOT}/share/${YINST_VAR_basePkg}/scripts/yjava_daemon_start.sh
 
