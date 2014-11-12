b933b0485451207a076d8ecd390240d1cb3c454e
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-12 15:13:50 -0600

    Adding new UT class file SocketUtil

---
+++ common/src/test/java/io/druid/common/utils/SocketUtilTest.java
@@ -0,0 +1,34 @@
+package io.druid.common.utils;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.metamx.common.ISE;
+
+/*
+ Testing the class SocketUtil
+ */
+
+public class SocketUtilTest{
+	
+	@Test
+	public void testSocketUtil() {
+		try{
+			Assert.assertTrue((SocketUtil.findOpenPort(0)<0xffff));
+		}catch(ISE expected){
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testIllegalArgument(){
+		SocketUtil.findOpenPort(-1);
+	}
+	
+	@Test(expected=ISE.class)
+	public void testISEexception(){
+		 SocketUtil.findOpenPort(0xffff);
+		 }
+}
+ 
