220b8d9f66af4bc4eb55550945a9562c6a0d2a4e
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-12 15:15:14 -0600

    Adding new UT class file VMUtils

---
+++ common/src/test/java/io/druid/common/utils/VMUtilsTest.java
@@ -0,0 +1,23 @@
+package io.druid.common.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class VMUtilsTest{
+	
+	@Test
+	public void testgetMaxDirectMemory(){
+
+		try{
+			long maxMemory=VMUtils.getMaxDirectMemory();
+			Assert.assertTrue((maxMemory>0));
+			
+			}catch( UnsupportedOperationException expected){
+				Assert.assertTrue(true);
+			}catch(RuntimeException expected){
+				Assert.assertTrue(true);
+			}
+	}
+	
+}
