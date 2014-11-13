be36d629f50cef159aec40ac573a413000695223
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-12 18:35:39 -0600

    fixing indentation

--- common/src/test/java/io/druid/common/utils/SocketUtilTest.java
+++ common/src/test/java/io/druid/common/utils/SocketUtilTest.java
@@ -6,29 +6,25 @@ import org.junit.Test;
 
 import com.metamx.common.ISE;
 
-/*
- Testing the class SocketUtil
- */
-
 public class SocketUtilTest{
-	
-	@Test
-	public void testSocketUtil() {
-		try{
-			Assert.assertTrue((SocketUtil.findOpenPort(0)<0xffff));
-		}catch(ISE expected){
-			Assert.assertTrue(true);
-		}
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testIllegalArgument(){
-		SocketUtil.findOpenPort(-1);
-	}
-	
-	@Test(expected=ISE.class)
-	public void testISEexception(){
-		 SocketUtil.findOpenPort(0xffff);
-		 }
+  
+  @Test
+  public void testSocketUtil() {
+    try{
+      Assert.assertTrue((SocketUtil.findOpenPort(0)<0xffff));
+    }catch(ISE expected){
+      Assert.assertTrue(true);
+    }
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testIllegalArgument(){
+    SocketUtil.findOpenPort(-1);
+  }
+  
+  @Test(expected=ISE.class)
+  public void testISEexception(){
+     SocketUtil.findOpenPort(0xffff);
+     }
 }
  
