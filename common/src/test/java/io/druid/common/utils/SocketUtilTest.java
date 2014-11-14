f735735510377fa35c3d2c71a598ad8e95ec5948
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-14 08:51:40 -0600

    Code style fix

--- common/src/test/java/io/druid/common/utils/SocketUtilTest.java
+++ common/src/test/java/io/druid/common/utils/SocketUtilTest.java
@@ -5,25 +5,28 @@ import org.junit.Test;
 
 import com.metamx.common.ISE;
 
-public class SocketUtilTest{
-  
+public class SocketUtilTest
+{
   @Test
-  public void testSocketUtil() {
-    try{
-      Assert.assertTrue((SocketUtil.findOpenPort(0)<0xffff));
-    }catch(ISE expected){
+  public void testSocketUtil()
+  {
+    try {
+      Assert.assertTrue((SocketUtil.findOpenPort(0) < 0xffff));
+    } catch (ISE expected) {
       Assert.assertTrue(true);
     }
   }
   
-  @Test(expected=IllegalArgumentException.class)
-  public void testIllegalArgument(){
+  @Test(expected = IllegalArgumentException.class)
+  public void testIllegalArgument()
+  {
     SocketUtil.findOpenPort(-1);
   }
   
-  @Test(expected=ISE.class)
-  public void testISEexception(){
-     SocketUtil.findOpenPort(0xffff);
-     }
+  @Test(expected = ISE.class)
+  public void testISEexception()
+  {
+    SocketUtil.findOpenPort(0xffff);
+  }
 }
  
--- common/src/test/java/io/druid/common/utils/VMUtilsTest.java
+++ common/src/test/java/io/druid/common/utils/VMUtilsTest.java
@@ -3,19 +3,18 @@ package io.druid.common.utils;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class VMUtilsTest{
-  
+public class VMUtilsTest
+{
   @Test
-  public void testgetMaxDirectMemory(){
-    try{
-      long maxMemory=VMUtils.getMaxDirectMemory();
-      Assert.assertTrue((maxMemory>0));
-      }
-    catch(UnsupportedOperationException expected){
+  public void testgetMaxDirectMemory()
+  {
+    try {
+      long maxMemory = VMUtils.getMaxDirectMemory();
+      Assert.assertTrue((maxMemory > 0));
+    } catch (UnsupportedOperationException expected) {
       Assert.assertTrue(true);
-      }
-    catch(RuntimeException expected){
+    } catch (RuntimeException expected) {
       Assert.assertTrue(true);
-      }
     }
+  }
 }
