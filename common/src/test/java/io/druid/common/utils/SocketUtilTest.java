98d41b9488329a3d694f24c968186ddd519cec1d
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-14 13:07:20 -0600

    Adding hamcrest ordering comparison api, this will provide a better error stack information in case the test fails.

--- common/src/test/java/io/druid/common/utils/SocketUtilTest.java
+++ common/src/test/java/io/druid/common/utils/SocketUtilTest.java
@@ -3,14 +3,18 @@ package io.druid.common.utils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.hamcrest.number.OrderingComparison;
 import com.metamx.common.ISE;
 
 public class SocketUtilTest
 {
+  private final int MAX_PORT=0xffff;
   @Test
   public void testSocketUtil()
   {
-    Assert.assertTrue((SocketUtil.findOpenPort(0) < 0xffff));
+    int port=SocketUtil.findOpenPort(0);
+    Assert.assertThat("Port is greater than the maximum port 0xffff",port, OrderingComparison.lessThanOrEqualTo(MAX_PORT));
+    Assert.assertThat("Port is less than minimum port 0",port, OrderingComparison.greaterThanOrEqualTo(0));
   }
 
   @Test(expected = IllegalArgumentException.class)
