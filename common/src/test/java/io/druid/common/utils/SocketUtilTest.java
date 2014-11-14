1213a21dcf5c4760c00d53747d5eb21c8b929323
Kyle Kloepper <kloepper@yahoo-inc.com>
2014-11-14 10:33:27 -0600

    Removing exception handling from unit test.

--- common/src/test/java/io/druid/common/utils/SocketUtilTest.java
+++ common/src/test/java/io/druid/common/utils/SocketUtilTest.java
@@ -10,23 +10,18 @@ public class SocketUtilTest
   @Test
   public void testSocketUtil()
   {
-    try {
-      Assert.assertTrue((SocketUtil.findOpenPort(0) < 0xffff));
-    } catch (ISE expected) {
-      Assert.assertTrue(true);
-    }
+    Assert.assertTrue((SocketUtil.findOpenPort(0) < 0xffff));
   }
-  
+
   @Test(expected = IllegalArgumentException.class)
   public void testIllegalArgument()
   {
     SocketUtil.findOpenPort(-1);
   }
-  
+
   @Test(expected = ISE.class)
   public void testISEexception()
   {
     SocketUtil.findOpenPort(0xffff);
   }
 }
- 
