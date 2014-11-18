7ed769630d49ea34695131dcb7d82e08b183607c
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-18 10:56:36 -0600

    Fixing codeStyle
    Fixing code style in CountingMapTest
    
    Fixing code style CountingMapTest
    
    More fixing codeStyle

--- common/src/test/java/io/druid/collections/CombiningIteratorTest.java
+++ common/src/test/java/io/druid/collections/CombiningIteratorTest.java
@@ -1,23 +1,24 @@
 package io.druid.collections;
 
 import com.google.common.collect.PeekingIterator;
+
 import com.metamx.common.guava.nary.BinaryFn;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.easymock.EasyMock;
+import org.junit.Test;
 
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
 public class CombiningIteratorTest
 {
-  private CombiningIterator <String> testingIterator;
-  private Comparator <String> comparator;
+  private CombiningIterator<String> testingIterator;
+  private Comparator<String> comparator;
   private BinaryFn fn;
-  private PeekingIterator <String> it ;
+  private PeekingIterator<String> it;
 
   @Before
   public void setUp() throws Exception
@@ -25,19 +26,19 @@ public class CombiningIteratorTest
     it = EasyMock.createMock(PeekingIterator.class);
     comparator = EasyMock.createMock(Comparator.class);
     fn = EasyMock.createMock(BinaryFn.class);
-    testingIterator=CombiningIterator.create(it,comparator,fn);
+    testingIterator = CombiningIterator.create(it,comparator,fn);
   }
 
   @After
   public void tearDown() throws Exception
   {
-    testingIterator=null;
+    testingIterator = null;
   }
 
   @Test
   public void testHasNext() throws Exception
   {
-    boolean expected=true;
+    boolean expected = true;
     EasyMock.expect(it.hasNext()).andReturn(expected);
     EasyMock.replay(it);
     boolean actual = testingIterator.hasNext();
@@ -48,9 +49,9 @@ public class CombiningIteratorTest
   @Test
   public void testFalseBranchNext() throws Exception
   {
-    boolean expected=true;
+    boolean expected = true;
     EasyMock.expect(it.hasNext()).andReturn(expected);
-    expected=false;
+    expected = false;
     EasyMock.expect(it.hasNext()).andReturn(expected);
     EasyMock.replay(it);
     Object res = testingIterator.next();
@@ -61,17 +62,21 @@ public class CombiningIteratorTest
   @Test
   public void testNext() throws Exception
   {
-    boolean expected=true;
+    boolean expected = true;
     EasyMock.expect(it.hasNext()).andReturn(expected).times(4);
-    String defaultString="S1";
-    String resString="S2";
+    String defaultString = "S1";
+    String resString = "S2";
     EasyMock.expect(it.next()).andReturn(defaultString);
-    EasyMock.expect(fn.apply(EasyMock.eq(defaultString), EasyMock.isNull())).andReturn(resString);
+    EasyMock.expect(fn.apply(EasyMock.eq(defaultString), EasyMock.isNull()))
+        .andReturn(resString);
     EasyMock.expect(it.next()).andReturn(defaultString);
-    EasyMock.expect(comparator.compare(EasyMock.eq(resString), EasyMock.eq(defaultString))).andReturn(0);
+    EasyMock.expect(comparator.compare(EasyMock.eq(resString), EasyMock.eq(defaultString)))
+        .andReturn(0);
     EasyMock.expect(it.next()).andReturn(defaultString);
-    EasyMock.expect(fn.apply(EasyMock.eq(resString), EasyMock.eq(defaultString))).andReturn(resString);
-    EasyMock.expect(comparator.compare(EasyMock.eq(resString), EasyMock.eq(defaultString))).andReturn(1);
+    EasyMock.expect(fn.apply(EasyMock.eq(resString), EasyMock.eq(defaultString)))
+        .andReturn(resString);
+    EasyMock.expect(comparator.compare(EasyMock.eq(resString), EasyMock.eq(defaultString)))
+        .andReturn(1);
 
     EasyMock.replay(it);
     EasyMock.replay(fn);
@@ -88,7 +93,7 @@ public class CombiningIteratorTest
   @Test(expected = NoSuchElementException.class)
   public void testExceptionInNext() throws Exception
   {
-    boolean expected=false;
+    boolean expected = false;
     EasyMock.expect(it.hasNext()).andReturn(expected);
     EasyMock.replay(it);
     testingIterator.next();
