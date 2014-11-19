06d4bc307f3745361020c2046f1a5b0ca0f9fddf
Himanshu Gupta <himanshg@yahoo-inc.com>
2014-11-19 10:32:20 -0600

    more tests for DbConnectorConfig getPassword()

--- common/src/test/java/io/druid/db/DummyPasswordProvider.java
+++ common/src/test/java/io/druid/db/DummyPasswordProvider.java
@@ -5,13 +5,18 @@ import java.util.Map;
 import io.druid.common.config.PasswordProvider;
 
 public class DummyPasswordProvider implements PasswordProvider {
-  
+
+  private String pwd;
+
   @Override
-  public void init(Map<String, String> config) {}
+  public void init(Map<String, String> config)
+  {
+    this.pwd = config.get("pwd");
+  }
 
   @Override
   public String getPassword()
   {
-    return "nothing";
+    return pwd;
   }
 }
