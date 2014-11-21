fd304c77c6998a20a6b7fbae0c123e0ab09a58b4
Slim Bouguerra <bslim@closesuppose-lm.champ.corp.yahoo.com>
2014-11-20 19:44:21 -0600

    Applying good recomendation from Kyle.

--- common/src/test/java/io/druid/collections/CombiningIterableTest.java
+++ common/src/test/java/io/druid/collections/CombiningIterableTest.java
@@ -2,48 +2,34 @@ package io.druid.collections;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
+import com.google.common.collect.Iterables;
 
+import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+
+import java.util.*;
 
 public class CombiningIterableTest
 {
-
   @Test
   public void testCreateSplatted()
   {
-    List<Integer> list1 = Arrays.asList(1, 2, 5, 7, 9);
-    List<Integer> list2 = Arrays.asList(2, 8);
-    List<Integer> list3 = Arrays.asList(4, 6, 8);
-    final ArrayList<Iterable<Integer>> iterators = Lists.newArrayList();
-    iterators.add(list1);
-    iterators.add(list2);
-    iterators.add(list3);
-    HashMap<Integer,String> expectedMap = new HashMap<>();
-    for (Iterable myIt : iterators) {
-      Iterator it = myIt.iterator();
-      while (it.hasNext()) {
-        expectedMap.put((Integer) it.next(),"1");
-      }
-    }
-    CombiningIterable<Integer> iter = CombiningIterable.createSplatted(
+    List<Integer> firstList = Arrays.asList(1, 2, 5, 7, 9, 10, 20);
+    List<Integer> secondList = Arrays.asList(1, 2, 5, 8, 9);
+    Set<Integer> mergedLists = new HashSet<>();
+    mergedLists.addAll(firstList);
+    mergedLists.addAll(secondList);
+    ArrayList<Iterable<Integer>> iterators = Lists.newArrayList();
+    iterators.add(firstList);
+    iterators.add(secondList);
+    CombiningIterable<Integer> actualIterable = CombiningIterable.createSplatted(
         iterators,
         Ordering.<Integer>natural()
     );
-
-    Iterator<Integer> it = iter.iterator();
-
-    while (it.hasNext()) {
-      Integer key = it.next();
-      Assert.assertTrue(expectedMap.containsKey(key));
-      expectedMap.remove(key);
-    }
-    Assert.assertEquals("size does not match", 0, expectedMap.size());
+    Assert.assertEquals(mergedLists.size(),Iterables.size(actualIterable));
+    Set actualHashset = Sets.newHashSet(actualIterable);
+    Assert.assertEquals(actualHashset,mergedLists);
   }
 }
