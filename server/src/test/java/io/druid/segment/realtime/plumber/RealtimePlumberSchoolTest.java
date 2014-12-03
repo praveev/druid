a68a603b1680e50e711f39dfc973504f5d810e47
Slim Bouguerra <bslim@yahoo-inc.com>
2014-12-03 09:25:20 -0600

    Removing the timeout of 1 second. This timeout does not make sense.

--- server/src/test/java/io/druid/segment/realtime/plumber/RealtimePlumberSchoolTest.java
+++ server/src/test/java/io/druid/segment/realtime/plumber/RealtimePlumberSchoolTest.java
@@ -65,6 +65,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
@@ -197,8 +198,11 @@ public class RealtimePlumberSchoolTest
   @Test
   public void testPersist() throws Exception
   {
-    final MutableBoolean committed = new MutableBoolean(false);
-    plumber.getSinks().put(0L, new Sink(new Interval(0, TimeUnit.HOURS.toMillis(1)),schema, tuningConfig, DateTime.now().toString()));
+    final CountDownLatch committed = new CountDownLatch(1);
+    plumber.getSinks().put(
+        0L,
+        new Sink(new Interval(0, TimeUnit.HOURS.toMillis(1)),schema, tuningConfig, DateTime.now().toString())
+    );
     plumber.startJob();
     final InputRow row = EasyMock.createNiceMock(InputRow.class);
     EasyMock.expect(row.getTimestampFromEpoch()).andReturn(0L);
@@ -211,18 +215,12 @@ public class RealtimePlumberSchoolTest
           @Override
           public void run()
           {
-            committed.setValue(true);
+            committed.countDown();
           }
         }
     );
 
-    Stopwatch stopwatch = Stopwatch.createStarted();
-    while (!committed.booleanValue()) {
-      Thread.sleep(100);
-      if (stopwatch.elapsed(TimeUnit.MILLISECONDS) > 1000) {
-        throw new ISE("Taking too long to set perist value");
-      }
-    }
+    committed.await();
     plumber.getSinks().clear();
     plumber.finishJob();
   }
