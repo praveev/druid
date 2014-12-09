0e57483e964f0388162b898e22997f5aa809d52a
Slim Bouguerra <bslim@yahoo-inc.com>
2014-12-09 17:26:55 -0600

    Unit test for class GranularUnprocessedPathSpec

---
+++ indexing-hadoop/src/test/java/io/druid/indexer/path/GranularUnprocessedPathSpecTest.java
@@ -0,0 +1,15 @@
+package io.druid.indexer.path;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GranularUnprocessedPathSpecTest
+{
+  @Test public void testSetGetMaxBuckets()
+  {
+    GranularUnprocessedPathSpec granularUnprocessedPathSpec = new GranularUnprocessedPathSpec();
+    int maxBuckets = 5;
+    granularUnprocessedPathSpec.setMaxBuckets(maxBuckets);
+    Assert.assertEquals(maxBuckets,granularUnprocessedPathSpec.getMaxBuckets());
+  }
+}
