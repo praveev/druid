c09f6d214c2b7c0fcb9d193bcd90be1cb4910482
Slim Bouguerra <bslim@yahoo-inc.com>
2014-12-18 13:40:37 -0600

    fixing style

--- server/src/test/java/io/druid/client/indexing/ClientConversionQueryTest.java
+++ server/src/test/java/io/druid/client/indexing/ClientConversionQueryTest.java
@@ -13,27 +13,31 @@ public class ClientConversionQueryTest
   private static final Interval INTERVAL = new Interval(new DateTime(), new DateTime().plus(1));
   private static final DataSegment DATA_SEGMENT = new DataSegment(DATA_SOURCE, INTERVAL, new DateTime().toString(), null,
       null, null, null, 0, 0);
-  
-  @Test public void testGetType()
+
+  @Test
+  public void testGetType()
   {
     clientConversionQuery = new ClientConversionQuery(DATA_SEGMENT);
     Assert.assertEquals("version_converter", clientConversionQuery.getType());
   }
 
-  @Test public void testGetDataSource()
+  @Test
+  public void testGetDataSource()
   {
     clientConversionQuery = new ClientConversionQuery(DATA_SEGMENT);
     Assert.assertEquals(DATA_SOURCE, clientConversionQuery.getDataSource());
 
   }
 
-  @Test public void testGetInterval()
+  @Test
+  public void testGetInterval()
   {
     clientConversionQuery = new ClientConversionQuery(DATA_SEGMENT);
     Assert.assertEquals(INTERVAL, clientConversionQuery.getInterval());
   }
 
-  @Test public void testGetSegment()
+  @Test
+  public void testGetSegment()
   {
     clientConversionQuery = new ClientConversionQuery(DATA_SEGMENT);
     Assert.assertEquals(DATA_SEGMENT, clientConversionQuery.getSegment());
