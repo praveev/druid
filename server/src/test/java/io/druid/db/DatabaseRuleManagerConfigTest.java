baa573649591949f0bae2fd6ca7aaace037267dc
Slim Bouguerra <bslim@yahoo-inc.com>
2014-12-19 17:00:12 -0700

    Testing serialization deserialization

--- server/src/test/java/io/druid/db/DatabaseRuleManagerConfigTest.java
+++ server/src/test/java/io/druid/db/DatabaseRuleManagerConfigTest.java
@@ -1,21 +1,30 @@
 package io.druid.db;
 
+import io.druid.jackson.JsonTestUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.joda.time.Period;
 
 public class DatabaseRuleManagerConfigTest
 {
-  DatabaseRuleManagerConfig  databaseRuleManagerConfig = new DatabaseRuleManagerConfig();
+  private final static String defaultRule = "_default";
+  private final static Period defaultPeriod = new Period("PT1M");
+  DatabaseRuleManagerConfig databaseRuleManagerConfig = new DatabaseRuleManagerConfig();
+  @Test
+  public void testSerializationDeSerialization() throws Exception
+  {
+    Assert.assertNotNull(JsonTestUtils.jsonWriteReadWrite(databaseRuleManagerConfig));
+  }
+
   @Test
   public void testGetDefaultRule()
   {
-    Assert.assertEquals("_default", databaseRuleManagerConfig.getDefaultRule());
+    Assert.assertEquals(defaultRule, databaseRuleManagerConfig.getDefaultRule());
   }
 
   @Test
   public void testGetPollDuration()
   {
-    Assert.assertEquals( new Period("PT1M"), databaseRuleManagerConfig.getPollDuration());
+    Assert.assertEquals(defaultPeriod, databaseRuleManagerConfig.getPollDuration());
   }
 }
