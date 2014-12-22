c0a942cb089c410cddbd46dac44c268b7daa9223
Himanshu Gupta <himanshg@yahoo-inc.com>
2014-12-22 17:25:45 -0600

    catching IOException just for pushTaskLog in order to accidentally not declare success on some other unintended IOException

--- indexing-service/src/test/java/io/druid/indexing/common/tasklogs/FileTaskLogsTest.java
+++ indexing-service/src/test/java/io/druid/indexing/common/tasklogs/FileTaskLogsTest.java
@@ -40,23 +40,32 @@ public class FileTaskLogsTest
     }
   }
 
-  @Test(expected = IOException.class)
+  @Test
   public void testPushTaskLogDirCreationFails() throws Exception
   {
     final File tmpDir = Files.createTempDir();
     
     try {
+      IOException thrown = null;
+      
       final File logDir = new File(tmpDir, "druid/logs");
       final File logFile = new File(tmpDir, "log");
       Files.write("blah", logFile, Charsets.UTF_8);
       
-      if(!tmpDir.setReadOnly()) {
+      if(!tmpDir.setWritable(false)) {
         new RuntimeException("failed to make tmp dir read-only");
       }
+
       final TaskLogs taskLogs = new FileTaskLogs(new FileTaskLogsConfig(logDir));
-      taskLogs.pushTaskLog("foo", logFile);
+      try {
+        taskLogs.pushTaskLog("foo", logFile);
+      } catch(IOException ex) {
+        thrown = ex;
+      }
+      Assert.assertNotNull("pushTaskLog should fail with exception of dir creation error", thrown);
     }
     finally {
+      tmpDir.setWritable(true);
       FileUtils.deleteDirectory(tmpDir);
     }
   }
